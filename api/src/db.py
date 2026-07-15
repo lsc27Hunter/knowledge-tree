0ab112c6ac98e20bdda5d68b1f9238376bb5d8e0
Riyad-0 <riyad.rahman73@login.cuny.edu>
2026-07-15 01:54:26 -0400

    set up backend testing

--- api/src/db.py
+++ api/src/db.py
@@ -2,21 +2,12 @@
 
 from typing import Annotated
 from fastapi import Depends
-from sqlalchemy import NullPool
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.ext.asyncio import AsyncSession
 
-from env import db_url, on_vercel
-
-if on_vercel:
-  # Can't use connection pooling in Vercel's serverless environment.
-  connect_args={
-    "prepared_statement_cache_size": 0,
-  }
-  engine = create_async_engine(db_url, echo=True, connect_args=connect_args, poolclass=NullPool)
-else:
-  engine = create_async_engine(db_url, echo=True)
+from engine import get_engine
 
 async def get_session():
+  engine = get_engine()
   async with AsyncSession(engine) as session:
     yield session
 
